71a1d44a2b8ac426e58940f79fb80cc4c968b287
Ryan Võ <thuan.vo@executionlab.asia>
2023-06-07 11:12:43 +0700

    fix _blank

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,26 +2,22 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    // footer have contact info(email, github, linkedin, facebook, phone number). They are all links and have icons, all in center and 80% width
     <footer className="flex items-center justify-center w-4/5 mx-auto my-8">
-      {/* <h1 className="text-2xl font-bold">Ryan&apos;s Portfolio</h1> */}
-
       <ul className="flex space-x-4">
         <li>
-          {/* mail in .env */}
           <Link href={`mailto:${process.env.NEXT_PUBLIC_MY_EMAIL || "/"}`}>
             Email
           </Link>
         </li>
         <li>
-          <Link href={process.env.NEXT_PUBLIC_MY_GITHUB || "/"} target="blank">
+          <Link href={process.env.NEXT_PUBLIC_MY_GITHUB || "/"} target="_blank">
             Github
           </Link>
         </li>
         <li>
           <Link
             href={process.env.NEXT_PUBLIC_MY_LINKEDIN || "/"}
-            target="blank"
+            target="_blank"
           >
             LinkedIn
           </Link>
@@ -29,7 +25,7 @@ export default function Footer() {
         <li>
           <Link
             href={process.env.NEXT_PUBLIC_MY_FACEBOOK || "/"}
-            target="blank"
+            target="_blank"
           >
             Facebook
           </Link>
